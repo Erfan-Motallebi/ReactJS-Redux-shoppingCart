47907f33f8601db1026ef1815324945ef900ab65
Erfan Motallebi <Erfan.FullDevs@gmail.com>
2021-07-06 11:22:46 +0430

    creating products components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
+import React, { Component } from "react";
 // feature-1
 import "./App.css";
+import productData from "./productData.json";
+import Products from "./components/Products";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: productData.product,
+      size: "",
+      sort: "",
+    };
+  }
 
-function App() {
-  return (
-    <div class="grid-container">
-      <header>
-        <h3>React Shopping Cart</h3>
-      </header>
-      <main>Body</main>
-      <footer>
-        <h4>All is reserved </h4>
-      </footer>
-    </div>
-  );
+  render() {
+    return (
+      <div class="grid-container">
+        <header>
+          <h3>React Shopping Cart</h3>
+        </header>
+        <main>
+          <div class="content">
+            <div className="main-content">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sideBar">SideBars</div>
+          </div>
+        </main>
+        <footer>
+          <h4>All is reserved </h4>
+        </footer>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/Products.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import currencyFormat from "../utils";
+
+export default class Products extends Component {
+  render() {
+    return (
+      <div>
+        <ul className="products">
+          {this.props.products.map((product) => {
+            return (
+              <li key={product._id}>
+                <div className="product">
+                  <a href={"#" + product._id}>
+                    <img src={product.image} alt={product.title} />
+                    <p>{product.title}</p>
+                  </a>
+                  <div className="product-price">
+                    <div>
+                      <h3>{currencyFormat(product.price)}</h3>
+                    </div>
+                    <button className="btn primary">Add To Cart </button>
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
